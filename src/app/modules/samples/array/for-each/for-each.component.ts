667f62d520fafd4ec642ecdba93be3f038b7fce3
Yasmany Montiel Rivera <yasmontiel87@gmail.com>
2020-12-12 14:19:27 +0100

    Trabajo con forEach Array

--- src/app/modules/samples/array/for-each/for-each.component.ts
+++ src/app/modules/samples/array/for-each/for-each.component.ts
@@ -13,6 +13,7 @@ export class ForEachComponent implements OnInit {
   }
 
   public onArrayIterator(): void {
+    console.log('Recorriendo el arreglo con  forEach()');
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
     numbers.forEach(item => {
       console.log('item-value: ', item);
@@ -20,8 +21,19 @@ export class ForEachComponent implements OnInit {
   }
 
   public onArrayReverse(): void {
+    console.log('Recorriendo inverso del arreglo con  reverse()');
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-    console.log(numbers.reverse());
+    const reverse = numbers.reverse();
+    reverse.forEach(item => {
+      console.log('item-value: ', item);
+    });
   }
 
+  public onArrayReverse1(): void {
+    console.log('Recorriendo inverso del arreglo con  for()');
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    for (let i = numbers.length - 1; i >= 0; i--) {
+      console.log('item-value: ', numbers[i]);
+    }
+  }
 }
